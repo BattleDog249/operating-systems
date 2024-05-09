c3e586cfb9779a770191cd4f4eb9cdda8ddeddb1
Logan Gray <lhgray@cmail.lcsc.edu>
2024-05-08 18:17:27 -0700

    Project 5 complete

--- project5/netmon.c
+++ project5/netmon.c
@@ -1,12 +1,12 @@
 #include <linux/module.h> // Required for all kernel modules
 #include <linux/kernel.h> // Required for printk, KERN_INFO, KERN_ERR
 #include <linux/init.h> // Required for __init and __exit macros
-#include <linux/netfilter.h>
-#include <linux/netfilter_ipv4.h>
+#include <linux/netfilter.h> // Required for netfilter hooks
 #include <linux/ip.h> // Required for iphdr
 #include <linux/proc_fs.h> // Required for the proc file system
 #include <linux/rbtree.h> // Required for Red-Black Trees
 #include <linux/slab.h> // Required for kmalloc, kfree
+#include <linux/rwsem.h> // Required for read-write semaphores
 
 #define PROC_FILENAME "ip_traffic"
 
@@ -14,12 +14,14 @@ static struct nf_hook_ops netfilter_ops_in;
 static struct proc_dir_entry *proc_file;
 struct rb_root ip_tree = RB_ROOT;
 
+// Define the structure for the Red-Black Tree nodes
 struct ip_node {
     struct rb_node node;
     u32 ip_addr;
     int count;
 };
 
+// Function to insert an IP address into the Red-Black Tree
 static void ip_tree_insert(u32 ip_addr) {
 	//printk(KERN_INFO "netmon: ip_tree_insert() called\n");
     struct ip_node *data, *tmp;
@@ -52,41 +54,65 @@ static void ip_tree_insert(u32 ip_addr) {
     rb_insert_color(&data->node, &ip_tree);
 }
 
+// Declare a read-write semaphore
+static DECLARE_RWSEM(ip_tree_rwsem);
+
+// Function to insert an IP address into the Red-Black Tree
 static unsigned int main_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-	//printk(KERN_INFO "netmon: main_hook() called\n");
     struct iphdr *ip;
 
     if (!skb)
         return NF_ACCEPT;
 
     ip = ip_hdr(skb);
+    down_write(&ip_tree_rwsem);
     ip_tree_insert(ntohl(ip->saddr));
+    up_write(&ip_tree_rwsem);
 
     return NF_ACCEPT;
 }
 
+// Function to read the contents of the Red-Black Tree
 static ssize_t read_proc(struct file *file, char __user *ubuf, size_t count, loff_t *ppos) {
-	//printk(KERN_INFO "netmon: Reading /proc file\n");
     int len = 0;
-    char buf[256];
     struct rb_node *node;
     struct ip_node *data;
+    int num_nodes = 0;
 
+    // Count the number of nodes in the tree
+    for (node = rb_first(&ip_tree); node; node = rb_next(node)) {
+        num_nodes++;
+    }
+
+    // Dynamically allocate the buffer based on the number of nodes
+    char *buf = kmalloc(num_nodes * 20, GFP_KERNEL); // Assume each node needs 20 bytes
+    if (!buf) {
+        printk(KERN_ERR "Unable to allocate buffer.\n");
+        return -ENOMEM;
+    }
+
+    down_read(&ip_tree_rwsem);
     for (node = rb_first(&ip_tree); node; node = rb_next(node)) {
         data = rb_entry(node, struct ip_node, node);
         u32 ip_addr_network_byte_order = htonl(data->ip_addr);
-        len += snprintf(buf + len, sizeof(buf) - len, "%pI4 - %d\n", &ip_addr_network_byte_order, data->count);
+        len += snprintf(buf + len, num_nodes * 20 - len, "%pI4 - %d\n", &ip_addr_network_byte_order, data->count);
         if (len > count)
             break;
     }
+    up_read(&ip_tree_rwsem);
 
-    if (*ppos > 0 || len == 0)
+    if (*ppos > 0 || len == 0) {
+        kfree(buf);
         return 0;
+    }
 
-    if (copy_to_user(ubuf, buf, len))
+    if (copy_to_user(ubuf, buf, len)) {
+        kfree(buf);
         return -EFAULT;
+    }
 
     *ppos = len;
+    kfree(buf);
     return len;
 }
 
@@ -94,6 +120,7 @@ static struct proc_ops netmon_proc_ops = {
     proc_read: read_proc,
 };
 
+// Function to initialize the module
 static int __init init_netmon(void) {
 	printk(KERN_INFO "netmon: Loading module\n");
     netfilter_ops_in.hook = main_hook;
@@ -107,6 +134,7 @@ static int __init init_netmon(void) {
     return 0;
 }
 
+// Function to clean up the module
 static void __exit exit_netmon(void) {
 	printk(KERN_INFO "netmon: Unloading module\n");
     struct rb_node *node;
